f0553af1d4f7bd890670d029cb8daf723119c59d
chansen-une <chansen9@myune.edu.au>
2026-06-12 13:23:31 +0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.00
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "this is a string"
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
